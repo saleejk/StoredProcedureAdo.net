be23c39f7bd42e6a8ccc4f714e4af417dd508afc
saleejk <saleejckd4ever@gmail.com>
2024-05-11 09:49:13 +0530

    Add project files.

---
+++ StoredProcedureAdo.net/Program.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.Identity.Client;
+using System.Data;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        //Program.SP_WithoutInput_and_Output();
+        Program.SP_WithInput_and_Without_Output(2);
+    }
+    public static void SP_WithoutInput_and_Output()
+    {
+        string connectionString = "data source= SALEEJK; database= master;integrated security=SSPI;TrustServerCertificate=True";
+        using(SqlConnection connection=new SqlConnection(connectionString))
+        {
+            connection.Open();
+            SqlCommand command = new SqlCommand("getFromStudent", connection);
+            command.CommandType = CommandType.StoredProcedure;
+            SqlDataReader reader=command.ExecuteReader();
+            while(reader.Read())
+            {
+                Console.WriteLine($"{reader[0]}{reader[1]}{reader[2]}");
+            }
+        }
+    }
+    //public static void SP_WithInput_and_Without_Output(int id)
+    //{
+    //    string connectionString= "data source= SALEEJK; database= master;integrated security=SSPI;TrustServerCertificate=True";
+    //    using(SqlConnection connection=new SqlConnection(connectionString))
+    //    {
+    //        connection.Open();
+    //        SqlCommand command = new SqlCommand("getById", connection);
+    //        command.CommandType = CommandType.StoredProcedure;
+    //        command.Parameters.AddWithValue("id", id);
+    //        SqlDataReader reader=command.ExecuteReader();
+    //        while(reader.Read())
+    //        {
+    //            Console.WriteLine($"{reader[0]}{reader[1]}{reader[2]}");
+    //        }
+    //    }
+    //}
+    public static void SP_WithInput_and_Without_Output(int id)
+    {
+        try
+        {
+            string connectionString= "data source= SALEEJK; database= master;integrated security=SSPI;TrustServerCertificate=True";
+            using(SqlConnection connection=new SqlConnection(connectionString))
+            {
+                connection.Open();
+                SqlCommand command = new SqlCommand("getById", connection);
+                command.CommandType = CommandType.StoredProcedure;
+                command.Parameters.AddWithValue("id", id);
+                SqlDataReader reader = command.ExecuteReader();
+                while (reader.Read())
+                {
+                    Console.WriteLine($"{reader[0]}{reader[1]}");
+                }
+            }
+        }catch(Exception ex)
+        {
+            Console.WriteLine($"failed to read {ex}");
+        }
+    }
+}
